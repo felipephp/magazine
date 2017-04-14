3f3059424a81666c18888ef981b4e45234b64df0
felipe <felipe.php@outlook.com>
2017-04-14 17:05:44 -0300

    Init Slim Skeleton and make most layout codes

---
+++ src/views/about.php
@@ -0,0 +1,74 @@
+<?php ?>
+<div>
+    <div class="text">
+        <div class="main-title bp">
+            <div>Dados Pessoais</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+
+    <div class="text">
+        <div class="main-title bp">
+            <div>Formação</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+
+    <div class="text">
+        <div class="main-title bp">
+            <div>Inglês</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+
+    <div class="text">
+        <div class="main-title bp">
+            <div>Hobbies</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+
+    <div class="text">
+        <div class="main-title bp">
+            <div>Objetivo</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+</div>
+
+<div>
+    <div class="text">
+        <div class="main-title op">
+            <div>Cursos</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+
+    <div class="text">
+        <div class="main-title op">
+            <div>Github</div>
+            <div></div>
+        </div>
+        <div>Texto aqui</div>
+    </div>
+</div>
+
+<div class="cleaner"></div>
+
+<div class="videos">
+    <h1>profissional</h1>
+    <iframe src="https://www.youtube.com/embed/r5z-lilm-gg" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<div class="videos">
+    <h1>pessoal</h1>
+    <iframe src="https://www.youtube.com/embed/wB8mr4iViy0" frameborder="0" allowfullscreen></iframe>
+</div>
+
+<div class="cleaner"></div>
---
+++ src/views/knowledge.php
@@ -0,0 +1,408 @@
+<?php ?>
+
+<div id="know">
+
+    <div class="main-title op">
+        <div>Avançado</div>
+        <div></div>
+    </div>
+
+    <div class="item">
+        <div class="title">PHP / OO MVC</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Laravel / Lumen / Slim Frameworks</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Eloquent</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">DB Relacional MySQL e Modelagem</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Javascript [puro] + JQuery</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">HTML + CSS</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Ambiente Linux</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Git Versionamento</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cleaner"></div>
+
+    <div class="main-title bp">
+        <div>Intermediário</div>
+        <div></div>
+    </div>
+
+    <div class="item">
+        <div class="title">Nodejs + MongoDB</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="item">
+        <div class="title">Angular, Vue, React . js</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Amazon AWS Setup</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cleaner"></div>
+
+    <div class="main-title op">
+        <div>Estudando</div>
+        <div></div>
+    </div>
+
+    <div class="item">
+        <div class="title">Golang</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Ionic 2</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Unit Tests [PHP Unit]</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cleaner"></div>
+
+    <div class="main-title op">
+        <div>Em Breve...</div>
+        <div></div>
+    </div>
+
+    <div class="item">
+        <div class="title">NoSQL Usages</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">Elastic Search e Bigdata</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="title">iOT (Arduino)</div>
+        <div class="logo">
+            <img src="#">
+        </div>
+        <div class="graphs">
+            <div class="circle-level" data-percent="0.7">
+                <span class="title">level</span>
+                <div class="info">
+                    <div>1</div> <!-- 04/2016 -->
+                    <div>ano(s)</div>
+                </div>
+            </div>
+            <div class="circle-xp" data-percent="0.8">
+                <span class="title">xp</span>
+                <div class="info">
+                    <div>80%</div> <!-- experiencia em projetos -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="cleaner"></div>
+</div>
+<!--desafiador-->
